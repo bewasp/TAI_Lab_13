30bb989bfcaa2fda8094e4ed31f9b0671f819725
bewasp <beewasp1997@gmail.com>
2020-03-28 14:17:48 +0100

    Zad 7

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BlogComponent,
-    ContactComponent
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
