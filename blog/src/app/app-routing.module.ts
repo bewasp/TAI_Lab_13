008176ab450b040da77c82462e10822b8fa8679c
bewasp <beewasp1997@gmail.com>
2020-03-28 14:00:02 +0100

    Zad 6

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {BlogComponent} from "./components/blog/blog.component";
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+
+}];
+
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
